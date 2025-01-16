693ec24062224a1e6cb02f26165f5b346f32c40c
K3ros3ne <uchebapochta@outlook.com>
2025-01-16 11:57:17 +0100

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,32 +3,22 @@ api_key = 'a023a3be26e530f37924110734b494b1'
 
 def fetch_weather_data(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        kelvin_temp = data["main"]["temp"]
-        celsius_temp = kelvin_temp - 273.15
-      
-        return round(celsius_temp, 2)
-    else:
-        return "Error: Unable to fetch data"
-
-from unittest.mock import patch, MagicMock
-
-def test_fetch_weather_data():
-    mock_response = {
-        "main": {
-            "temp": 293.15 
-        }
-    }
     
-    with patch("requests.get") as mock_get:
-        mock_get.return_value = MagicMock(ok=True, status_code=200, json=MagicMock(return_value=mock_response))
-        
-        assert fetch_weather_data("Prague") == 20.0
+    try:
+        response = requests.get(url)
+        if response.ok:
+            data = response.json()
+            temp_kelvin = data['main']['temp']
+            temp_celsius = temp_kelvin - 273.15
+            return round(temp_celsius, 2)
+        else:
+            return "error"
+    except requests.exceptions.RequestException:
 
 if __name__ == "__main__":
     city = input("Enter city name: ")
-    temperature = fetch_weather_data(city)
-    print(f"Current temperature in {city}: {temperature} °C")
+    if city:
+        temperature = fetch_weather_data(city)
+        print(f"Current temperature in {city}: {temperature} °C")
+    else:
+        print("City name cannot be empty.")
